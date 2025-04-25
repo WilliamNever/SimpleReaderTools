06e6c3327aa73b1cb43eab9dc8e5753e11c076d5
William <willnever97@hotmail.com>
2025-04-25 09:55:24 +0800

    add duplicated running checking

--- SimpleReaderTools/Program.cs
+++ SimpleReaderTools/Program.cs
@@ -1,7 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Threading.Tasks;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
 using System.Windows.Forms;
 
 namespace SimpleReaderTools
@@ -12,11 +13,40 @@ namespace SimpleReaderTools
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
-        static void Main()
+        static void Main(params string[] args)
         {
+            if ((args?.Length ?? 0) > 0)
+            {
+                if (CheckUniqueRunning(args))
+                {
+                    if (MessageBox.Show("This Application is in running.\r\nCreate another new Process?", 
+                        "Info -", MessageBoxButtons.YesNo)
+                        == DialogResult.No)
+                    {
+                        Environment.Exit(0);
+                        return;
+                    }
+                }
+            }
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        public static bool CheckUniqueRunning(string[] args)
+        {
+            if (args.Any(x => "-unique".Equals(x, StringComparison.OrdinalIgnoreCase)
+            || "-u".Equals(x, StringComparison.OrdinalIgnoreCase)))
+            {
+                var assemlyName = Assembly.GetExecutingAssembly().GetName().Name;
+                var curretProcessId = Process.GetCurrentProcess().Id;
+                var excutionList = Process.GetProcesses().Select(x => new { x.Id, x.ProcessName });
+                return excutionList.
+                    Any(
+                    x => x.Id != curretProcessId
+                    && (x.ProcessName ?? "").Equals(assemlyName, StringComparison.OrdinalIgnoreCase));
+            }
+            return false;
         }
     }
 }
